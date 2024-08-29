c57a7125808f4f7780d0705ddf4b0614653e9a87
SHEHROZ03 <shehrozmalik033@gmail.com>
2024-08-29 20:29:47 +0500

    new

--- class.py
+++ class.py
@@ -17,8 +17,26 @@ print("my favourite animal is also",animal, "!")
 #new program of converting fahrenheit into Celsius
 
 # taking input from user in fahrenheit
-fahrenheit = float(input("Enter temprature in fahrenheit: "))
+fahrenheit = float(input("Enter temperature in fahrenheit: "))
 #converting fahrenheit into celsius
 celsius = (fahrenheit - 32) *5/9
 #displaying the result
-print(f"tempraure in fahrenheit is: {fahrenheit} F tempraure in celsius is: {celsius} C")
+print(f"temperature in fahrenheit is: {fahrenheit} F temperature in celsius is: {celsius} C")
+
+#new program of printing the perimeter of triangle
+side1 = float(input("Enter first side of triangle :"))
+side2 = float(input("Enter second side of triangle :"))
+side3 = float(input("Enter third side of triangle :"))
+
+#calculating the perimeter
+perimeter = side1 + side2 + side3
+
+#printing the perimeter of triangle
+print("The perimeter of triangle is : ",perimeter)
+
+#new program of printing squre of a number
+number = int(input("Enter the number :"))
+#calculating the square 
+num = number ** 2
+#printing the square of a number
+print("The square of",number, "is :",num)
