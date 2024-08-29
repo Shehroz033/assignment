a7fb7f1813663b762776200e816c7eafec0d33ee
Shehroz malik <shehrozmalik033@gmail.com>
2024-08-29 20:02:49 +0500

    Add files via upload

---
+++ class.py
@@ -0,0 +1,24 @@
+
+num1 = int (input("enter num1 :"))
+num2 = int(input("enter num2 :"))
+#adding two number given by user
+sum = num1+num2
+#printing sum of two number given by user
+print("The sum of ",num1,"and",num2,"is",sum)
+
+#new program of printing favourite animal of user
+
+#taking input from user to print favourite animal
+animal = (input("what is your favourite animal "))
+
+#printing user favourite animal
+print("my favourite animal is also",animal, "!")
+
+#new program of converting fahrenheit into Celsius
+
+# taking input from user in fahrenheit
+fahrenheit = float(input("Enter temprature in fahrenheit: "))
+#converting fahrenheit into celsius
+celsius = (fahrenheit - 32) *5/9
+#displaying the result
+print(f"tempraure in fahrenheit is: {fahrenheit} F tempraure in celsius is: {celsius} C")
