931845b36202b9c00d0fc85c6c52c1f76e7ccd85
Shehroz033 <shehrozmalik033@gmail.com>
2024-09-24 22:54:54 +0500

    function assignment

---
+++ calculater2.py
@@ -0,0 +1,43 @@
+# calculator in python using functions
+# addition function 
+
+def add(x,y):
+    return(x+y)
+
+# subtraction function
+
+def subtract(x,y):
+    return(x-y)
+
+# multiplication function
+
+def multiply(x,y):
+    return(x*y)
+
+# division function
+
+def dividing(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+num1 = float(input("Enter the num 1 : "))
+num2 = float(input("Enter the num 2 : "))
+ 
+ #selecting the operation
+
+operation = input("Choose an operation (+, -, *, /): ")
+
+# performing the selected operation
+
+if operation == "+":
+    print(f"Result: {add(num1, num2)}")
+elif operation == "-":
+    print(f"Result: {subtract(num1, num2)}")
+elif operation == "*":
+    print(f"Result: {multiply(num1, num2)}")
+elif operation == "/":
+    print(f"Result: {dividing(num1, num2)}")
+else:
+    print("Invalid operation")
